c43fe6b51c025cdce9d99b047dd9924cc9a8f29d
Ariana Goode <arianagoode@Arianas-MacBook-Pro.local>
2016-11-24 13:05:30 -0500

    First Commit

---
+++ Buddah.java
@@ -0,0 +1,7 @@
+class Buddah{
+	public static void main(String[] args) {
+	//I want buddah to say "Inhale"
+	System.out.println("Inhale the good shit :)") ;
+	System.out.println("Exhale the bullshit!");
+    }
+}
