3035bb0efc737f83cf7a7e2def4fe6f8d24bb658
Ariana Goode <agoode43@gmail.com>
2017-02-27 00:00:06 -0500

    Delete buddha.java

--- buddha.java
+++
@@ -1,8 +0,0 @@
-class buddha{
-	public static void main(String[] args) {
-	/*I want buddha to say "Inhale the good shit(smiley face)" and 
-	"Exhale the bullshit!" on two separate lines*/
-	System.out.println("Inhale the good shit :)") ;
-	System.out.println("Exhale the bullshit!");
-    }
-}
