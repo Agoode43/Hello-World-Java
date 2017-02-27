0da1978afb6954067ca4ee33215c87175fde095f
Ariana Goode <arianagoode@Arianas-MacBook-Pro.local>
2017-02-26 23:42:02 -0500

    Hello-World-Java

---
+++ NamePrinter.java
@@ -0,0 +1,8 @@
+class Ariana{
+	//Goal is to print my name inside of a box
+	public static void main(String[] args) {
+		System.out.println("+------+");
+		System.out.println("|Ariana|");
+		System.out.println("+------+");
+	}
+}
